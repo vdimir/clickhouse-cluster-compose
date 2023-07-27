191e550bd3b42b091a9bcdba228d0644196f204b
vdimir <vdimir@clickhouse.com>
2023-07-27 14:50:15 +0000

    Use config.d

--- chcluster/entrypoint.sh
+++ chcluster/entrypoint.sh
@@ -4,4 +4,4 @@ mkdir -p /var/lib/clickhouse/
 
 echo runnning ${APP_TYPE:-server} ${CHREPLICA:-}
 
-/clickhouse ${APP_TYPE:-server} --config-file=/etc/clickhouse-config.xml
+/clickhouse ${APP_TYPE:-server} --config-file=/etc/clickhouse/config.xml
